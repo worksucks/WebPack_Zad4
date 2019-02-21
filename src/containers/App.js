53f743653dcbc64b5fc65965a314807a63836262
Tomasz Reizer <t_reizer@yahoo.com>
2019-02-21 18:19:33 +0100

    added catalogs src build

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import style from './App.css';
 import Title from '../components/Title.js';
+import TodoList from '../components/TodoList.js'
 //import TodoList from './components/TodoList.js';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            data: [{
+                id: 1,
+                text: 'clean room'
+              }, {
+                id: 2,
+                text: 'wash the dishes'
+              }, {
+                id: 3,
+                text: 'feed my cat'
+              }]
         };
     }
     addTodo(val){
@@ -29,6 +39,7 @@ class App extends React.Component {
           <div className={style.TodoApp}>
               Tutaj pojawią się komponenty naszej aplikacji.
               <Title/>
+              <TodoList/>
               </div>
       );
     }
